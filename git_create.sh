5be8036a1b9d63c51afb7b2b31b5ba7441504b27
Sebastian Lyng Johansen <seb.l.j@hotmail.com>
2020-02-12 18:01:37 +0100

    added possibility to change url to ssh, and now search for username and not name in gitconfig

--- git_create.sh
+++ git_create.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-USERNAME=$(git config user.name)
+USERNAME=$(git config user.username)
 TOKEN=$(git config user.token)
 REPONAME=""
 DESCRIPTION=""
 NARGS=$#
 FLAG=$1
-SSH=$5
+URL="HTTPS"
 
-if [ $NARGS != 1 ] && [ $NARGS != 5 ];then
+if [ $NARGS != 1 ] && [ $NARGS != 5 ] && [ $NARGS != 4 ];then
 	echo Wrong use of git create, try -h for help
 	exit 1
 fi
@@ -16,13 +16,13 @@ fi
 if [ $FLAG = "-h" ];then
 	echo git create is dependent on a working git config
 	echo Syntax for git create:"\n"
-	echo git create [-flag] [repo name] [description] [public"(y/n)"] [ssh"(y/n)"]"\n"
+	echo git create [-flag] [repo name] [description] [public"(y/n)"] [ssh "(optional)"]"\n"
 	echo -flag:"\n"-n "->" create a new git repository 
 	exit 1
 fi
 
 if [ $FLAG = "-n" ];then
-	if [ $NARGS != 4 ];then
+	if [ $NARGS != 4 ] && [ $NARGS != 5 ];then
 		echo missing arguments
 		exit 1
 	fi
@@ -53,15 +53,28 @@ if [ $FLAG = "-n" ];then
     	case $yn in
         	[Yy]* ) yn="true" break;;
         	[Nn]* ) yn="false" break;;
-        * ) echo "Please answer yes or no."; read yn; continue;
+			* ) echo "Please answer if you want public repo. (y/n)"; read yn; continue;
     	esac
 	done
+
+	if [ $NARGS = 5 ]; then
+		URL=$5
+		while true; do
+			case $URL in
+				SSH|ssh ) URL="SSH" break;;
+				HTTPS|https ) URL="HTTPS" break;;
+				* ) echo "Please answer which URL you want (ssh/https)"; read URL; continue;
+			esac
+		done	
+			
+	fi
 
 	echo Repository name : $REPONAME
 	DESCRIPTION=$3
 	echo Description : $DESCRIPTION
 	PUBLIC=$yn
 	echo Public : $PUBLIC
+	echo URL : $URL
 fi
 
 LINK=https://github.com/${USERNAME}/${REPONAME}.git
@@ -75,4 +88,8 @@ git commit -m "first commit"
 git remote add origin ${LINK}
 git push -u origin master
 
+if [ $URL = "SSH" ] || [ $URL = "ssh" ]; then
+
+	git remote set-url origin git@github.com:${USERNAME}/${REPONAME}
+fi
 
