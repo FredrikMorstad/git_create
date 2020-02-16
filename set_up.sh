e427600a8b073295e2af4a7992727a9739ddcd86
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2020-02-16 17:29:06 +0100

    update

--- set_up.sh
+++ set_up.sh
@@ -4,9 +4,11 @@ root=$(pwd)
 rcfile=.bashrc
 file=git_create.sh
 
+
 cp ${root}/${file} ${HOME}
 
 cd
+echo Moving file to the home directory..;echo
 
 mv git_create.sh .git_create.sh
 
@@ -14,15 +16,15 @@ file=.git_create.sh
 
 currentdir=$(pwd)
 
-a=${currentdir}
-
 if ! (grep "alias git-create="\"${currentdir}/$file"" ${rcfile} >/dev/null); then
-echo adding alias in bashrc...
+echo adding alias in bashrc...;echo
+
 cat >> $rcfile <<EOF
 alias git-create="${currentdir}/${file}"
 EOF
-
 fi
+echo Looks like you already have a git-create alias;echo
 
 cd $root
+echo Reloading the bash files
 bash
